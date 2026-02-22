c0e0ae78759ad0b5e83d448b0cdc727c22794d47
Sjnai <Sjnai@192.168.0.18>
2026-02-22 22:00:42 +0000

    project initial setup

---
+++ src/main/java/com/testAutomation/apiTestAutomation/App.java
@@ -0,0 +1,10 @@
+package com.testAutomation.apiTestAutomation;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
