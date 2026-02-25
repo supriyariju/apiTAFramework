63584f284ef45404cc700a50e3a2ca354939da0f
Sjnai <Sjnai@192.168.0.18>
2026-02-25 13:09:16 +0000

    Second test commit

--- src/main/java/com/testAutomation/apiTestAutomation/App.java
+++ src/main/java/com/testAutomation/apiTestAutomation/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!!!!");
         System.out.println("Hello World!! Its a test commit.");
+        System.out.println("Hello World!! Its a test commit2.");
     }
 }
 
